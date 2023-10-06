b3e23d32c768137829f1cc61a63af7d5b8440b9c
James W. Crofts <jcrofts@pobox.com>
2023-10-06 12:09:07 -0700

    Enable std support in guest

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,6 +1,6 @@
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std]  // std support is experimental
+//#![no_std]  // std support is experimental
 
 
 use risc0_zkvm::guest::env;
