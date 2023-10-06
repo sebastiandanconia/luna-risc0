54f8446e8646e770122b4af5ac78986299fed011
James W. Crofts <jcrofts@pobox.com>
2023-10-05 20:32:01 -0700

    Change guest project name to "luna"

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,7 +1,7 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
-use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
+use methods::{LUNA_ELF, LUNA_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv};
 
 fn main() {
@@ -21,12 +21,12 @@ fn main() {
     let prover = default_prover();
 
     // Produce a receipt by proving the specified ELF binary.
-    let receipt = prover.prove_elf(env, METHOD_NAME_ELF).unwrap();
+    let receipt = prover.prove_elf(env, LUNA_ELF).unwrap();
 
     // TODO: Implement code for transmitting or serializing the receipt for
     // other parties to verify here
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
-    receipt.verify(METHOD_NAME_ID).unwrap();
+    receipt.verify(LUNA_ID).unwrap();
 }
